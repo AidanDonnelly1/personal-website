4b1d5bfebde9c1a827cc0f5fff69ca7b5321f9a1
AidanDonnelly1 <aido4381@colorado.edu>
2024-03-25 17:20:03 -0600

    back to bare bones

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -71,9 +71,9 @@
     </section>
     <section class="py-20 lg:py-32 flex flex-col gap-24" id="projects">
         <div class="flex flex-col gap-2 text-center">
-            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+            <!-- <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
                 Curious to <span class="poppins text-violet-400">see</span> my work?
-            </h3>
+            </h3> -->
         </div>
        
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
