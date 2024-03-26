af784db3c60a169e5989b7b49cb2d964acee1a01
AidanDonnelly1 <aido4381@colorado.edu>
2024-03-25 21:58:05 -0600

    created gallery; need to add other content; maybe expand gallery

--- src/components/about/Gallery.svelte
+++ src/components/about/Gallery.svelte
@@ -5,49 +5,44 @@
      */
      export let image_src;
 </script>
-<div class="grid grid-cols-2 md:grid-cols-3 gap-2">
-    <div class="grid gap-2">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[0]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[1]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[2]} alt="">
-        </div>
+
+
+
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[0]} alt="">
     </div>
-    <div class="grid gap-2">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[3]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[4]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[5]} alt="">
-        </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[1]} alt="">
     </div>
-    <div class="grid gap-2">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[6]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[7]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[8]} alt="">
-        </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[2]} alt="">
     </div>
-    <!-- <div class="grid gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src={image_src[0]} alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
-        </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[3]} alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[4]} alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[5]} alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[6]} alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[7]} alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[8]} alt="">
+    </div>
+    <!-- <div>
+        <img class="h-auto max-w-full rounded-lg" src={image_src[9]} alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt="">
+    </div>
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt="">
     </div> -->
 </div>
